62977aa799fb0009a7dc86f28f076c6aea56244d
LaurentBaj <laurent_baj@hotmail.com>
2020-08-23 12:31:50 +0200

    Added aditional rescources for PG4200

--- PGR203 - Advanced Java Programming/Exercices/Ex1/NumsToRomanNums/src/main.java
+++ PGR203 - Advanced Java Programming/Exercices/Ex1/NumsToRomanNums/src/main.java
@@ -1,6 +1,6 @@
 public class main {
     public static void main(String[] args) {
         obj a = new obj();
-        System.out.println(a.toRoman(39));
+        System.out.println(a.toRoman(12));
     }
 }
--- PGR203 - Advanced Java Programming/Exercices/Ex1/NumsToRomanNums/src/obj.java
+++ PGR203 - Advanced Java Programming/Exercices/Ex1/NumsToRomanNums/src/obj.java
@@ -2,30 +2,39 @@ public class obj {
     public String toRoman(int number) {
         String res = "";
 
-        do {
-            while (number >= 10 && number < 40) {
-                number -= 10;
-                res += "X";
-            }
-            if (number == 9) {
-                number -= 9;
-                res += "IX";
-            }
-            if (number >= 5) {
-                number -= 5;
-                res += "V";
-            }
-            if (number == 4) {
-                number -= 4;
-                res += "IV";
-            }
 
-            for (int i = 0; i < number; number--) {
-                res += "I";
-            }
+        while (number >= 10 && number < 40) {
+            res += "X";
+            number -= 10;
+        }
 
-        } while(number > 0);
-
+        if (number == 9) {
+            number -= 9;
+            res += "IX";
+        }
+        if (number >= 5) {
+            number -= 5;
+            res += "V";
+        }
+        if (number == 4) {
+            number -= 4;
+            res += "IV";
+        }
+        for (int i = 0; i < number; number--) {
+            res += "I";
+        }
         return res;
     }
+
+    public String decaKill(int num){
+        String r = "";
+
+        while (num >= 10 && num < 40) {
+            r += "X";
+            num -= 10;
+        }
+        return r;
+    }
 }
+
+
