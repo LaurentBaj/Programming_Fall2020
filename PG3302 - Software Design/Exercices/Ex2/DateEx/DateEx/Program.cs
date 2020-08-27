191eae4de4a3f862b32cd599c3c56be4b7095cbc
LaurentBaj <laurent_baj@hotmail.com>
2020-08-27 23:46:25 +0200

    Coding exercices on C#

---
+++ PG3302 - Software Design/Exercices/Ex2/DateEx/DateEx/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace DateEx
+{
+    public class Date
+    {
+        private int _month = 1;
+        public int Month { 
+            get { return _month; }
+            set 
+            {
+                if(value <= 0) {
+                    _month = 1; 
+                }
+                else if(value >= 12) {
+                    _month = 12; 
+                }
+                else {
+                    _month = value; 
+                }
+            }
+        }
+    }
+
+    class Person
+    {
+        private String _name = "Ukjent";  
+        private String _lastName = "Ukjent";  
+        public String Name
+        {
+            get { return _name; }
+            set 
+            {
+                _name = value; 
+            }
+
+        }
+        public String LastName
+        {
+            get { return _lastName; }
+            set
+            {
+                _lastName = value;
+            }
+        }
+
+        public String FullName
+        {
+            get { return _name + " " + _lastName; }
+        }
+    }
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Date d1 = new Date();
+            d1.Month = 3; 
+            Console.WriteLine(d1.Month);
+
+            Person p1 = new Person();
+            Console.WriteLine(p1.FullName);
+            p1.Name = "Laurent"; 
+            p1.LastName = "Bajrami";
+            Console.WriteLine(p1.FullName); 
+
+            Console.ReadKey(); 
+        }
+    }
+}
---
+++ PG3302 - Software Design/Exercices/Ex2/RandomNames/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace RandomNames
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random r = new Random();
+            String[] nameArray = { "Ben", "Stian", "Magnus", "Kai", "Sindre", "Adolf" };
+
+            foreach (String name in nameArray)
+            {
+                int rand = r.Next(0, nameArray.Length);
+                String randomName = nameArray[rand];
+                Console.WriteLine(randomName); 
+            }
+
+            Console.ReadLine(); 
+        }
+    }
+}
