97dd43bbb3b695c0c9bf67ec56a554b4a55768da
LaurentBaj <laurent_baj@hotmail.com>
2020-08-27 19:21:27 +0200

    PG3302
    Made a domain model diagram + played around with C#

---
+++ PG3302 - Software Design/Exercices/Ex2/ConsoleApp1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int b = 3;
+            String name = "Laurent";
+            char male = 'm';
+            double dec = 4.5;
+            bool isCool = true; 
+
+
+            Console.WriteLine("   /|");
+            Console.WriteLine("  / |");
+            Console.WriteLine(" /  |");
+            Console.WriteLine("/___|");
+
+            Console.WriteLine(b); 
+            Console.WriteLine(name); 
+            Console.WriteLine(male); 
+            Console.WriteLine(dec); 
+            Console.WriteLine(isCool);
+
+            //Readline keeps the program running though it has executed the entire program
+            Console.ReadLine(); 
+        }
+    }
+}
