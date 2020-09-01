70bbf86cdfea48f4fe3ae295dbcf771150d8cf4b
LaurentBaj <laurent_baj@hotmail.com>
2020-09-01 23:30:36 +0200

    Learned some basic C# methods

---
+++ PG3302 - Software Design/Exercices/Ex2/Brackeys C-sharp Course/Slide1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Slide1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1, num2;
+            
+            // Readline is the value of user input
+            // The content saved from a readline() is a string and needs to be converted to int 
+            // Convert.toInt32() converts a string int to integer int 
+
+            Console.Write("Add number to be multiplicated: ");
+            num1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Add a second number which * the initial number: ");
+            num2 = Convert.ToInt32(Console.ReadLine());
+
+            int res = num1 * num2;
+            Console.WriteLine("\n" + res); 
+        }
+    }
+}
