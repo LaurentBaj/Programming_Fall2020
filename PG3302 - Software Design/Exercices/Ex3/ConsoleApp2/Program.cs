d89d9a10437d9ecd60d9e078a6df188a85478f28
LaurentBaj <laurent_baj@hotmail.com>
2020-09-05 14:46:53 +0200

    Notes + Guessing game on C#

---
+++ PG3302 - Software Design/Exercices/Ex3/ConsoleApp2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random randomNumber = new Random();
+            int answer, userInput; 
+            string userInputFail;
+
+            do
+            {
+                answer = randomNumber.Next(1, 10);
+
+                Console.Write("Guess a number between one and ten:   ");
+                userInput = Convert.ToInt32(Console.ReadLine());
+                if (userInput == answer)
+                {
+                    Console.WriteLine("Correct!");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Wrong! Do you want to try again? (y/n)   ");
+                    userInputFail = Console.ReadLine();
+                    if (userInputFail == "n") break;
+                }
+
+            } while (userInput != answer); 
+
+
+            Console.ReadLine(); 
+        }
+    }
+}
