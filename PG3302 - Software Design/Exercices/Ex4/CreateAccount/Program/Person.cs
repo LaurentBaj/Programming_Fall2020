663e290551bf8ffc12f8c2f2c04b172382facbb9
LaurentBaj <laurent_baj@hotmail.com>
2020-09-11 13:54:48 +0200

    SOLID- SRP-Intro
    Made a program that creates an account name based on the combination of the first letter of your first name and the rest of your last name. The code is currently messy, but I intend to follow SRP to make it more adaptive

---
+++ PG3302 - Software Design/Exercices/Ex4/CreateAccount/Program/Person.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Program
+{
+    public class Person
+    {
+        public string FirstName; 
+        public string LastName;
+    }
+}
