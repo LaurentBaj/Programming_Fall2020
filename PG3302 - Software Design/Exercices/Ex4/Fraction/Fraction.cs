d3aeac7ec1c08cc5fe066dcee8dc06abfd797114
LaurentBaj <laurent_baj@hotmail.com>
2020-09-12 23:44:01 +0200

    Added relational operators
    They do not work yet

--- PG3302 - Software Design/Exercices/Ex4/Fraction/Fraction.cs
+++ PG3302 - Software Design/Exercices/Ex4/Fraction/Fraction.cs
@@ -15,33 +15,84 @@ namespace Fraction
             this.denominator = denominator;
         }
 
-        // Operator Overloading 
+        // Operator Overloads -- Arithmetic 
         public static Fraction operator +(Fraction f1, Fraction f2)
         {
             // Given that the denominators for each fraction have the same value 
             return new Fraction(f1.numerator + f2.numerator, f1.denominator);
         }
-
         public static Fraction operator -(Fraction f1, Fraction f2)
         {
             return new Fraction(f1.numerator - f2.numerator, f1.denominator);
         }
-
         public static Fraction operator *(Fraction f1, Fraction f2)
         {
             return new Fraction(f1.numerator * f2.numerator, f1.denominator * f2.denominator);
         }
-
         public static Fraction operator /(Fraction f1, Fraction f2)
         {
             return new Fraction(f1.numerator * f2.denominator, f1.denominator * f2.numerator);
         }
 
-        public static void Main()
+
+        // Operator Overloads -- Relational 
+        public static bool operator ==(Fraction f1, Fraction f2)
         {
-            var a = new Fraction(5, 4);
-            var b = new Fraction(1, 4);
-            Console.WriteLine(a + b);    
+            if(f1 == f2)
+            {
+                return true; 
+            } 
+        }
+        public static bool operator !=(Fraction f1, Fraction f2)
+        {
+            if (f1 != f2)
+            {
+                return true;
+            }
+            return false;
+        }
+        public static bool operator <(Fraction f1, Fraction f2)
+        {
+            if (f1 < f2)
+            {
+                return true;
+            }
+            return false;
+        }
+        public static bool operator >(Fraction f1, Fraction f2)
+        {
+            if (f1 > f2)
+            {
+                return true;
+            }
+            return false;
+        }
+        public static bool operator <=(Fraction f1, Fraction f2)
+        {
+            if (f1 <= f2)
+            {
+                return true;
+            }
+            return false;
+        }
+        public static bool operator >=(Fraction f1, Fraction f2)
+        {
+            if (f1 >= f2)
+            {
+                return true;
+            }
+            return false; 
+        }
+
+        // Methods
+        public override string ToString()
+        {
+            return numerator + "/" + denominator;
+        }
+
+        public double ToDecimal()
+        {
+            return numerator / (double)denominator; 
         }
     }
 }
---
+++ PG3302 - Software Design/Exercices/Ex4/Fraction/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Fraction
+{
+    class Program
+    {
+        public static void Main()
+        {
+            var a = new Fraction(2, 4);
+            var b = new Fraction(1, 4);
+
+            // Test arithmetic operators
+            Console.WriteLine(a + b);
+            Console.WriteLine(a - b);
+            Console.WriteLine(a * b);
+            Console.WriteLine(a / b);
+
+            // Test if it returns decimal version
+            Console.WriteLine(a.ToDecimal());
+
+            // Test relational operators 
+            Console.WriteLine(a == b);
+            Console.WriteLine(a != b);
+            Console.WriteLine(a > b);
+            Console.WriteLine(b < a);
+            Console.WriteLine(a >= b);
+            Console.WriteLine(b <= a);
+
+            Console.ReadLine();
+        }
+    }
+}
