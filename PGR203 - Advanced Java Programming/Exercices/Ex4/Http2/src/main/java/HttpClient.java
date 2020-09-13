5463cc917cbcf2d87b63219ebebfd509a31e6af3
LaurentBaj <laurent_baj@hotmail.com>
2020-09-13 21:53:57 +0200

    Both tests pass

--- PGR203 - Advanced Java Programming/Exercices/Ex4/Http2/src/main/java/HttpClient.java
+++ PGR203 - Advanced Java Programming/Exercices/Ex4/Http2/src/main/java/HttpClient.java
@@ -3,36 +3,38 @@ import java.net.Socket;
 
 public class HttpClient
 {
-    private String hostName, requestTarget;
-    private int port;
+    private final int responseCode;
 
-    public HttpClient(String hostName, int port, String requestTarget) {
-        this.hostName = hostName;
-        this.port = port;
-        this.requestTarget = requestTarget;
-    }
+    public HttpClient(String hostName, int port, String requestTarget) throws IOException {
+        Socket socket = new Socket(hostName, port);
 
-    public static void main(String[] args) throws IOException {
-        Socket socket = new Socket("urlecho.appspot.com", 80);
-        String request= "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9\r\n" +
-                "Accept-Language: en-GB,en-US;q=0.9,en;q=0.8,nb;q=0.7\r\n" +
-                "Cache-Control: max-age=0\r\n" +
-                "Connection: keep-alive\r\n" +
-                "dnt: 1\r\n" +
-                "Host: urlecho.appspot.com\r\n" +
-                "Upgrade-Insecure-Requests: 1\r\n" +
-                "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.102 Safari/537.36";
+        String request = "GET " + requestTarget + " HTTP/1.1\r\n" +
+                "Host: " + hostName + "\r\n\r\n";
+
         socket.getOutputStream().write(request.getBytes());
 
-
+        StringBuilder line = new StringBuilder();
         int c;
-        while ((c = socket.getInputStream().read()) != '\r') {
-            StringBuilder str = new StringBuilder();
-            str.append(c); 
+        while ((c = socket.getInputStream().read()) != -1)
+        {
+            if (c == '\n') break;
+            line.append((char) c);
         }
+        System.out.println(line);
+        String[] responseLineParts = line.toString().split(" ");
+        responseCode = Integer.parseInt(responseLineParts[1]);
     }
 
-    public Object getStatusCode() {
-        return 200;
+    public static void main(String[] args) throws IOException
+    {
+        String hostName = "urlecho.appspot.com";
+        int port = 80;
+        String requestTarget = "/echo?status=200&body=Hello%20world!";
+        new HttpClient(hostName, port, requestTarget);
+    }
+
+    public Object getStatusCode()
+    {
+        return responseCode;
     }
 }
