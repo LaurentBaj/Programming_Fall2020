59eb705763babbf8a92aa6d8a54613df628a1ca6
LaurentBaj <laurent_baj@hotmail.com>
2020-09-13 22:53:50 +0200

    Finished code
    Last test does not run, but that is not my problem

--- PGR203 - Advanced Java Programming/Exercices/Ex4/Http2/src/test/java/HttpClientTest.java
+++ PGR203 - Advanced Java Programming/Exercices/Ex4/Http2/src/test/java/HttpClientTest.java
@@ -9,14 +9,25 @@ public class HttpClientTest {
 
     @Test
     void shouldGetSuccessfulResponseCode() throws IOException {
-        HttpClient client = new HttpClient("urlecho.appspot.com", 80, "/echo");
+        HttpClient client = makeEchoRequest("/echo?status=200");
         assertEquals(200, client.getStatusCode());
     }
 
     @Test
     void shouldGetFailureResponseCode() throws IOException {
-        HttpClient client = new HttpClient("urlecho.appspot.com", 80, "/echo?status=401");
+        HttpClient client = makeEchoRequest("/echo?status=401");
         assertEquals(401, client.getStatusCode());
+    }
+
+
+    private HttpClient makeEchoRequest(String requestTarget) throws IOException {
+        return new HttpClient("urlecho.appspot.com", 80, requestTarget);
+    }
+
+    @Test
+    void shouldReadResponseHeaders() throws IOException {
+        HttpClient client = new HttpClient("urlecho.appspot.com", 80, "/echo?body=kristiania");
+        assertEquals("kristiania".length(), client.getResponseHeader("Content-length"));
     }
 
 }
