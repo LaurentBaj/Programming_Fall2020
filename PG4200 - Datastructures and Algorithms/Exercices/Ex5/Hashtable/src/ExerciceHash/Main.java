f9c847170f9abe0817a4104d46404b65676cd73a
LaurentBaj <laurent_baj@hotmail.com>
2020-09-25 00:31:35 +0200

    Hashtables intro: Finished

--- PG4200 - Datastructures and Algorithms/Exercices/Ex5/Hashtable/src/ExerciceHash/Main.java
+++ PG4200 - Datastructures and Algorithms/Exercices/Ex5/Hashtable/src/ExerciceHash/Main.java
@@ -3,6 +3,19 @@ package ExerciceHash;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("");
+        Employee e1 = new Employee("Laurent", "Bajrami", 333);
+        Employee e2 = new Employee("Ben", "Bajrami", 334);
+        Employee e3 = new Employee("Gard", "Jørstad", 222);
+        Employee e4 = new Employee("Alexander", "Gustavson", 311);
+        Employee e5 = new Employee("Emil", "Skolt", 234);
+
+        SimpleHashTable ht = new SimpleHashTable();
+        ht.put("Bajrami", e1);
+        ht.put("Bajrami", e2);
+        ht.put("Jørstad", e3);
+        ht.put("Gustavson", e4);
+        ht.put("Skolt", e5);
+
+        ht.printHashtable();
     }
 }
--- PG4200 - Datastructures and Algorithms/Exercices/Ex5/Hashtable/src/ExerciceHash/SimpleHashTable.java
+++ PG4200 - Datastructures and Algorithms/Exercices/Ex5/Hashtable/src/ExerciceHash/SimpleHashTable.java
@@ -7,7 +7,27 @@ public class SimpleHashTable {
         hashTable = new Employee[10];
     }
 
+    public void put(String key, Employee employee) {
+        int hashedKey = hashKey(key);
+        if (hashTable[hashedKey] != null) { // Handles collision in this case
+            System.out.println("Sorry, there's already an emplyee at position " + hashedKey);
+        } else {
+            hashTable[hashedKey] = employee;
+        }
+    }
+
+    public Employee get(String key) {
+        int hashedKey = hashKey(key);
+        return hashTable[hashedKey];
+    }
+
     private int hashKey(String key) {
         return key.length() % hashTable.length;
+    }
+
+    public void printHashtable() {
+        for (int i = 0; i < hashTable.length; i++) {
+            System.out.println(hashTable[i]);
+        }
     }
 }
