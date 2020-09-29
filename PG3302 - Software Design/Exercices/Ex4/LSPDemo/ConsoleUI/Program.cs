d63aff1091bb2ec206dc7202569712bbb014a09a
LaurentBaj <laurent_baj@hotmail.com>
2020-09-29 23:43:05 +0200

    Liskov Substitution principle

---
+++ PG3302 - Software Design/Exercices/Ex4/LSPDemo/ConsoleUI/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using LSPDemo;
+
+namespace DemoLibrary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Manager accountingVP = new Manager();
+
+            accountingVP.FirstName = "Emma";
+            accountingVP.LastName = "Stone"; 
+            accountingVP.CalculatePerHourRate(4);
+            
+            Employee emp = new Employee();
+
+            emp.FirstName = "Laurent";
+            emp.LastName = "Bajrami"; 
+            emp.AssignManager(accountingVP);
+            emp.CalculatePerHourRate(2);
+            
+            Console.WriteLine($"{ emp.FirstName}'s salary is ${ emp.Salary }/hour");
+
+            Console.ReadLine(); 
+        }
+    }
+}
