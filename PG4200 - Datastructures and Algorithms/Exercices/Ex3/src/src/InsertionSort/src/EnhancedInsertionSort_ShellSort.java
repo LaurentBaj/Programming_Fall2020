f3d2499bab6034c6ea809cbadf12f25fb93eb2cc
LaurentBaj <laurent_baj@hotmail.com>
2020-10-12 18:00:48 +0200

    Theory on Recursion (rep)

--- PG4200 - Datastructures and Algorithms/Exercices/Ex3/src/src/InsertionSort/src/EnhancedInsertionSort_ShellSort.java
+++ PG4200 - Datastructures and Algorithms/Exercices/Ex3/src/src/InsertionSort/src/EnhancedInsertionSort_ShellSort.java
@@ -1,34 +1,31 @@
 public class EnhancedInsertionSort_ShellSort {
 
     public static void main(String[] args) {
-
          int[] array = {-22, 55, 7, -15, 20, 35, 5};
 
-         for (int gap = array.length/2; gap > 0; gap /= 2) {    // Preliminary work by reducing gap
 
-             for (int i = gap; i < array.length; i++) { // Gaping Insertion sort (last iteration it is a trad Insertion)
-                 int newElement = array[i];             // because the gap will then decrease to one == Insertion Sort
+        for (int gap = array.length/2; gap > 0; gap /= 2) {    // Preliminary work by reducing gap
 
-                 int j = i;
+            for (int i = gap; i < array.length; i++) {         // Gaping Insertion sort (last iteration it is a trad Insertion)
+                int newElement = array[i];                     // because the gap will then decrease to one == Insertion Sort
 
-                 while (j >= gap && array[j - gap] > newElement) {
-                     array[j] = array[j- gap];
-                     j -= gap;
-                 }
+                int j = i;
 
-                 array[j] = newElement;
+                while (j >= gap && array[j - gap] > newElement) {
+                    array[j] = array[j- gap];
+                    j -= gap;
+                }
 
-             }
+                array[j] = newElement;
 
-         }
+            }
 
-
+        }
 
          // Output
          for (int i = 0; i < array.length; i++) {
              System.out.println(array[i]);
          }
-
 
     }
 
